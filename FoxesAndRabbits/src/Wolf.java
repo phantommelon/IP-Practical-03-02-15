f1ec0d4e9ce636ba87b421afa5e042e56494ab35
Alistair Madden <phantommelon@gmail.com>
2015-02-03 16:41:52 +0000

    Add Wolf class
    The Wolf class is essentially a refactored Fox. Change Simulator class
    to accomodate.

---
+++ FoxesAndRabbits/src/Wolf.java
@@ -0,0 +1,205 @@
+
+/*
+ * Copyright 2015 Alistair Madden <phantommelon@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.List;
+import java.util.Iterator;
+import java.util.Random;
+
+/**
+ * A simple model of a Wolf.
+ * Wolves age, move, eat rabbits/foxes, and die.
+ * 
+ * @author David J. Barnes and Michael Kolling edited by Alistair Madden
+ * @version 2008.03.30, 2015.02.03
+ */
+public class Wolf extends Animal {
+    // Characteristics shared by all wolves (static fields).
+    
+    // The age at which a wolf can start to breed.
+    private static final int BREEDING_AGE = 10;
+    // The age to which a wolf can live.
+    private static final int MAX_AGE = 150;
+    // The likelihood of a wolf breeding.
+    private static final double BREEDING_PROBABILITY = 0.35;
+    // The maximum number of births.
+    private static final int MAX_LITTER_SIZE = 5;
+    // The food value of a single rabbit. In effect, this is the
+    // number of steps a wolf can go before it has to eat again.
+    private static final int RABBIT_FOOD_VALUE = 7;
+    // The food value of a single fox. In effect, this is the
+    // number of steps a wolf can go before it has to eat again.
+    private static final int FOX_FOOD_VALUE = 14;
+    // A shared random number generator to control breeding.
+    private static final Random rand = Randomizer.getRandom();
+    
+    // Individual characteristics (instance fields).
+    // The wolf's age.
+    private int age;
+    // The wolf's food level, which is increased by eating rabbits.
+    private int foodLevel;
+
+    /**
+     * Create a Wolf. A wolf can be created as a new born (age zero
+     * and not hungry) or with a random age and food level.
+     * 
+     * @param randomAge If true, the wolf will have random age and hunger level.
+     * @param field The field currently occupied.
+     * @param location The location within the field.
+     */
+    public Wolf(boolean randomAge, Field field, Location location)
+    {
+        super(field, location);
+        if(randomAge) {
+            age = rand.nextInt(MAX_AGE);
+            foodLevel = rand.nextInt(RABBIT_FOOD_VALUE);
+        }
+        else {
+            age = 0;
+            foodLevel = RABBIT_FOOD_VALUE;
+        }
+    }
+    
+    /**
+     * This is what the wolf does most of the time: it hunts for
+     * rabbits. In the process, it might breed, die of hunger,
+     * or die of old age.
+     * @param field The field currently occupied.
+     * @param newWolves A list to add newly born wolves to.
+     */
+    public void act(List<Animal> newWolves)
+    {
+        incrementAge();
+        incrementHunger();
+        if(isAlive()) {
+            giveBirth(newWolves);            
+            // Move towards a source of food if found.
+            Location location = getLocation();
+            Location newLocation = findFood(location);
+            if(newLocation == null) { 
+                // No food found - try to move to a free location.
+                newLocation = getField().freeAdjacentLocation(location);
+            }
+            // See if it was possible to move.
+            if(newLocation != null) {
+                setLocation(newLocation);
+            }
+            else {
+                // Overcrowding.
+                setDead();
+            }
+        }
+    }
+
+    /**
+     * Increase the age. This could result in the wolf's death.
+     */
+    private void incrementAge()
+    {
+        age++;
+        if(age > MAX_AGE) {
+            setDead();
+        }
+    }
+    
+    /**
+     * Make this wolf more hungry. This could result in the wolf's death.
+     */
+    private void incrementHunger()
+    {
+        foodLevel--;
+        if(foodLevel <= 0) {
+            setDead();
+        }
+    }
+    
+    /**
+     * Tell the wolf to look for rabbits adjacent to its current location.
+     * Only the first live rabbit is eaten.
+     * @param location Where in the field it is located.
+     * @return Where food was found, or null if it wasn't.
+     */
+    private Location findFood(Location location)
+    {
+        Field field = getField();
+        List<Location> adjacent = field.adjacentLocations(getLocation());
+        Iterator<Location> it = adjacent.iterator();
+        while(it.hasNext()) {
+            Location where = it.next();
+            Object animal = field.getObjectAt(where);
+            if(animal instanceof Rabbit) {
+                Rabbit rabbit = (Rabbit) animal;
+                if(rabbit.isAlive()) { 
+                    rabbit.setDead();
+                    foodLevel = RABBIT_FOOD_VALUE;
+                    // Remove the dead rabbit from the field.
+                    return where;
+                }
+            }
+            if(animal instanceof Fox) {
+                Fox fox = (Fox) animal;
+                if(fox.isAlive()) { 
+                    fox.setDead();
+                    foodLevel = RABBIT_FOOD_VALUE;
+                    // Remove the dead rabbit from the field.
+                    return where;
+                }
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * Check whether or not this wolf is to give birth at this step.
+     * New births will be made into free adjacent locations.
+     * @param newWolves A list to add newly born wolves to.
+     */
+    private void giveBirth(List<Animal> newWolves)
+    {
+        // New wolves are born into adjacent locations.
+        // Get a list of adjacent free locations.
+        Field field = getField();
+        List<Location> free = field.getFreeAdjacentLocations(getLocation());
+        int births = breed();
+        for(int b = 0; b < births && free.size() > 0; b++) {
+            Location loc = free.remove(0);
+            Wolf young = new Wolf(false, field, loc);
+            newWolves.add(young);
+        }
+    }
+        
+    /**
+     * Generate a number representing the number of births,
+     * if it can breed.
+     * @return The number of births (may be zero).
+     */
+    private int breed()
+    {
+        int births = 0;
+        if(canBreed() && rand.nextDouble() <= BREEDING_PROBABILITY) {
+            births = rand.nextInt(MAX_LITTER_SIZE) + 1;
+        }
+        return births;
+    }
+
+    /**
+     * A wolf can breed if it has reached the breeding age.
+     */
+    private boolean canBreed()
+    {
+        return age >= BREEDING_AGE;
+    }
+}
